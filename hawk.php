6183a68e54ef0bd6a83316d0dae48a298e8597f3
N0str <ntpcp@yandex.ru>
2017-06-10 14:41:14 +0300

    Change statics

--- hawk.php
+++ hawk.php
@@ -1,19 +1,19 @@
 <?php
 
+require_once("config.php");
+
 
 class HawkErrorManager
 {
     # Define error handlers and load configuration
-    function __construct() {
+    public static function init() {
         set_error_handler(array('HawkErrorManager', 'Log'), E_ALL);
         set_exception_handler(array('HawkErrorManager', 'LogException'));
         error_reporting(E_ALL | E_STRICT);
-
-        $this->config = require_once("config.php");
     }
 
     # Construct logs package and send them to service with access token
-    public function Log($errno, $errstr, $errfile, $errline, $errcontext) {
+    public static function Log($errno, $errstr, $errfile, $errline, $errcontext) {
 
         $data = array(
             "error_type" => $errno,
@@ -24,16 +24,16 @@ class HawkErrorManager
             "debug_backtrace" => debug_backtrace(),
 
             # Access token obtained from official website
-            "access_token" => $this->config['access_token']
+            "access_token" => HawkConfig::$ACCESS_TOKEN
         );
 
-        $this->send($data);
+        HawkErrorManager::send($data);
 
     }
 
     # Construct Exceptions and send them to Logs
     public static function LogException($exception) {
-        self::Log(E_ERROR, $exception->getMessage(), $exception->getFile(), $exception->getLine(), []);
+        HawkErrorManager::Log(E_ERROR, $exception->getMessage(), $exception->getFile(), $exception->getLine(), []);
     }
 
     /*******************/
@@ -41,11 +41,11 @@ class HawkErrorManager
     /*******************/
 
     # Send package to service defined by api_url from settings
-    private function send($package) {
+    private static function send($package) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->config['api_url']);
+        curl_setopt($ch, CURLOPT_URL, HawkConfig::$API_URL);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($package));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($package));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
         curl_close ($ch);
