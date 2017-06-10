5b5a97df0f8561edf45380399ab3d3badf95a17e
N0str <ntpcp@yandex.ru>
2017-06-10 14:06:26 +0300

    Add configurations

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
     require_once 'hawk.php';
     HawkErrorManager::init();
 
+    echo "Script started";
     $a = 1 / 0;
-    $b = str_replace('test', 'test');
+//    $b = str_replace('test', 'test');
 
-    throw new Exception('Exception');
+//    throw new Exception('Exception');
+    echo "Script finished";
 ?>
-
