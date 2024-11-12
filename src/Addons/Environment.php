c124abf1a94fab12c835479c12449b1f5173c943
Pavel Zotikov <pavelzotikov@gmail.com>
2024-11-12 15:33:49 +0300

    linter

--- src/Addons/Environment.php
+++ src/Addons/Environment.php
@@ -40,4 +40,3 @@ class Environment implements AddonInterface
         }
     }
 }
-
--- src/Catcher.php
+++ src/Catcher.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Hawk;
 
-use Hawk\Addons\Headers;
 use Hawk\Addons\Environment;
+use Hawk\Addons\Headers;
 use Hawk\Transport\CurlTransport;
 use Throwable;
 
